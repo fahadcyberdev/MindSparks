06a34ab9043e3c35137444b37a36be01c94f7603
fahadcyberdev <fahadhosen.dev.19@gmail.com>
2025-06-12 11:42:20 +0600

    fix: Update default server port from 3000 to 4000

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 app.get('/' , (req, res) => res.send("Api is working"))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=> {
     console.log('server is running on port ' + PORT);
