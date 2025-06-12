a0712d0ed083d3c0af073fe4e3cf62f96de694c6
fahadcyberdev <fahadhosen.dev.19@gmail.com>
2025-06-12 11:38:53 +0600

    fix: Correct server startup logic and improve API response

--- server/server.js
+++ server/server.js
@@ -11,4 +11,15 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
+
+app.get('/' , (req, res) => res.send("Api is working"))
+
 const PORT = process.env.PORT ||  3000 ;
+
+
+app.listen(PORT, ()=> {
+    console.log('sever is runnig on port' + PORT);
+    
+})
+
+export default app; 
